194f856eb665da0a3c5d8b1abe53cf8ef92820c8
Daniel Ezomo <>
2023-12-28 19:34:08 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,17 @@
+sudo apt-get update && sudo apt-get upgrade;
+sudo apt-get install zsh;
+
+# エラー回避
+touch ~/.zshrc;
+sh -c "$(curl -fsSL https://git.io/zinit-install)";
+cp .zshrc -f ~/.zshrc;
+source ~/.zshrc;
+zinit self-update;
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k;
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc;
+
+cat setings.txt >> ~/.zshrc;
+
+chsh -s /usr/bin/zsh;
+reboot;

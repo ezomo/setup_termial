7d88af1fe651d848666d2e3f16a0359cef523701
ezomo <77780804+ezomo@users.noreply.github.com>
2024-01-28 22:11:50 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ apt-get update;
 echo "yes" | sudo apt-get upgrade;
 echo "yes" | apt-get install zsh;
 
-# \u30a8\u30e9\u30fc\u56de\u907f
+
 touch ~/.zshrc;
 sh -c "$(curl -fsSL https://git.io/zinit-install)";
 cp .zshrc -f ~/.zshrc;
@@ -14,9 +14,7 @@ echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc;
 
 cat settings.txt >> ~/.zshrc;
 
-apt-get install vim;
+echo "yes" |apt-get install vim;
 cp -f .vimrc  ~/.vimrc;
 
 chsh -s /usr/bin/zsh;
-source ~/.zshrc;
-zsh;

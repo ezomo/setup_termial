7ac5a60aca2e96fdaff4dbf7aa3258548631aeab
ezomo <77780804+ezomo@users.noreply.github.com>
2024-02-01 18:12:31 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
-apt-get update;
+echo "yes" | sudo apt-get update;
 echo "yes" | sudo apt-get upgrade;
-echo "yes" | apt-get install zsh;
+echo "yes" | sudo apt-get install zsh;
+echo "yes" | sudo apt-get install vim;
 
 
 touch ~/.zshrc;
 sh -c "$(curl -fsSL https://git.io/zinit-install)";
 cp .zshrc -f ~/.zshrc;
 source ~/.zshrc;
-zinit self-update;
+sudo zinit self-update;
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k;
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc;
 
 cat settings.txt >> ~/.zshrc;
 
-echo "yes" |apt-get install vim;
 cp -f .vimrc  ~/.vimrc;
 
 chsh -s /usr/bin/zsh;
